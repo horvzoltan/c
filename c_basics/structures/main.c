18b5758b23f73b9eb471b42c10c07fccf58b368b
horvzoltan <zoltanhorvath0721@gmail.com>
2024-05-10 21:34:51 +0200

    text editor in c

--- c_basics/structures/main.c
+++ c_basics/structures/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+struct person
+{
+    char *name;
+    int age;
+};
+
 int main()
 {
-    // aksjdklajd
-    // askdjaskdjalsdjka
-    // dasdkaskdja
+    // Now you can use the person struct
+    struct person john;
+
+    /* testing code */
+    john.name = "John";
+    john.age = 27;
+    printf("%s is %d years old.", john.name, john.age);
+
+    return 0;
 }
