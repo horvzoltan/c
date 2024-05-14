ecc39a6f81df1609b68adcb8b14a42492dfa2c98
horvzoltan <zoltanhorvath0721@gmail.com>
2024-05-14 23:25:12 +0200

    editorrefresh

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -6,13 +6,14 @@
 #include <ctype.h>
 #include <errno.h>
 
-/*** includes ***/
+/*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+/*** terminal ***/
 struct termios orig_termios;
-
 void die(const char *s)
 {
+    editorRefreshScreen();
     perror(s);
     exit(1);
 }
@@ -22,8 +23,8 @@ void disableRawMode()
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
         die("tcsetattr");
 }
-void enableRawMode()
 
+void enableRawMode()
 {
     if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
         die("tcgetattr");
@@ -39,24 +40,46 @@ void enableRawMode()
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
         die("tcsetattr");
 }
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((read(STDIN_FILENO, &c, 1) != 1))
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+/*** output ***/
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        editorRefreshScreen();
+        exit(0);
+        break;
+    }
+}
+
+/*** init ***/
 int main()
 {
     enableRawMode();
     while (1)
     {
-        char c = '\0';
-        if ((read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN))
-            die("read");
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q'))
-            break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
 
     return 0;
